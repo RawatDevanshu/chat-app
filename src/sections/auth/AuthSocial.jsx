d1fbc45147615c382ba83d74b476249ca959f7ec
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-03-10 22:04:08 +0530

    login and register form

---
+++ src/sections/auth/AuthSocial.jsx
@@ -0,0 +1,36 @@
+import { Divider, IconButton, Stack } from "@mui/material";
+import { GithubLogo, GoogleLogo, TwitterLogo } from "phosphor-react";
+import React from "react";
+
+const AuthSocial = () => {
+  return (
+    <div>
+      <Divider
+        sx={{
+          my: 2.5,
+          typography: "overline",
+          color: "text.disable",
+          "&::before, ::after": {
+            borderTopStyle: "dashed",
+          },
+        }}
+      >
+        OR
+      </Divider>
+      <Stack direction="row" justifyContent="center" spacing={2}>
+        <IconButton color="inherit">
+          <GithubLogo />
+        </IconButton>
+
+        <IconButton>
+          <GoogleLogo color="#df3e30" />
+        </IconButton>
+        <IconButton>
+          <TwitterLogo color="#1c9cea" />
+        </IconButton>
+      </Stack>
+    </div>
+  );
+};
+
+export default AuthSocial;
---
+++ src/sections/auth/RegisterForm.jsx
@@ -0,0 +1,113 @@
+import {
+  Stack,
+  Alert,
+  InputAdornment,
+  IconButton,
+  Button,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { RHFTextField } from "../../components/hook-form";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { Eye, EyeSlash } from "phosphor-react";
+
+const RegisterForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const RegisterSchema = Yup.object().shape({
+    firstName: Yup.string().required("First Name is required"),
+    lastName: Yup.string().required("Last Name is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be valid email address"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const defaultValues = {
+    firstName: "",
+    lastName: "",
+    email: "demo@tawk.com",
+    password: "demo1234",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(RegisterSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      //submit data to backend
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+        <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+          <RHFTextField name="firstName" label="First Name" />
+          <RHFTextField name="lastName" label="Last Name" />
+        </Stack>
+        <RHFTextField name="email" label="Email" />
+        <RHFTextField
+          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          fullWidth
+          color="inherit"
+          size="large"
+          type="submit"
+          variant="contained"
+          sx={{
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+            "&:hover": {
+              bgcolor: "text.primary",
+              color: (theme) =>
+                theme.palette.mode === "light" ? "common.white" : "grey.800",
+            },
+          }}
+        >
+          Login
+        </Button>
+      </Stack>
+    </FormProvider>
+  );
+};
+
+export default RegisterForm;
