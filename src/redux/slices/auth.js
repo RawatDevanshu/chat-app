6baa407fc29e443797fcaf72ecdd75b103c6e27d
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-03-26 20:26:09 +0530

    added new slices
    register user and verify email by otp slices

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -5,12 +5,18 @@ const initialState = {
   isLoggedIn: false,
   token: "",
   isLoading: false,
+  email: "",
+  error: false,
 };
 
 const slice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    updateIsLoading(state, action) {
+      state.error = action.payload.error;
+      state.isLoading = action.payload.isLoading;
+    },
     logIn(state, action) {
       state.isLoggedIn = action.payload.isLoggedIn;
       state.token = action.payload.token;
@@ -18,6 +24,9 @@ const slice = createSlice({
     signOut(state, action) {
       state.isLoggedIn = false;
       state.token = "";
+    },
+    updateRegisterEmail(state, action) {
+      state.email = action.payload.email;
     },
   },
 });
@@ -91,6 +100,70 @@ export function ResetPassword(formValues) {
     await axios
       .post(
         "auth/reset-password",
+        {
+          ...formValues,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(
+          slice.actions.logIn({
+            isLoggedIn: true,
+            token: response.data.token,
+          }),
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
+
+export function RegisterUser(formValues) {
+  return async (dispatch, getState) => {
+    await axios
+      .post(
+        "auth/register",
+        {
+          ...formValues,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(
+          slice.actions.updateRegisterEmail({ email: formValues.email }),
+        );
+        dispatch(
+          slice.actions.updateIsLoading({ isLoading: false, error: false }),
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        if (!getState().auth.error) {
+          window.location.href = "/auth/verify";
+        }
+      });
+  };
+}
+
+export function VerifyEmail(formValues) {
+  return async (dispatch, getState) => {
+    console.log(formValues);
+    await axios
+      .post(
+        "/auth/verify-otp",
         {
           ...formValues,
         },
